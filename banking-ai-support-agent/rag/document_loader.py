ca2045ef3e30116a6ffccc1245a1c814b8eac142
sandeepgirase55 <sandeep.girase@acldigital.com>
2026-05-10 22:06:14 +0530

    Added safe PDF and TXT loader

--- banking-ai-support-agent/rag/document_loader.py
+++ banking-ai-support-agent/rag/document_loader.py
@@ -1,21 +1,16 @@
 import os
+import logging
 
 from langchain_community.document_loaders import (
-    TextLoader
+    TextLoader,
+    PyPDFLoader
 )
 
 # =========================================
 # KNOWLEDGE BASE PATH
 # =========================================
 
-BASE_DIR = os.path.dirname(
-    os.path.abspath(__file__)
-)
-
-KNOWLEDGE_BASE_PATH = os.path.join(
-    BASE_DIR,
-    "knowledge_base"
-)
+KNOWLEDGE_BASE_PATH = "rag/knowledge_base"
 
 # =========================================
 # LOAD DOCUMENTS
@@ -25,24 +20,60 @@ def load_documents():
 
     documents = []
 
+    # CREATE FOLDER IF MISSING
+    os.makedirs(
+        KNOWLEDGE_BASE_PATH,
+        exist_ok=True
+    )
+
     for file_name in os.listdir(
         KNOWLEDGE_BASE_PATH
     ):
 
-        if file_name.endswith(".txt"):
+        file_path = os.path.join(
+            KNOWLEDGE_BASE_PATH,
+            file_name
+        )
 
-            file_path = os.path.join(
-                KNOWLEDGE_BASE_PATH,
-                file_name
+        try:
+
+            # =====================================
+            # TXT FILES
+            # =====================================
+
+            if file_name.endswith(".txt"):
+
+                loader = TextLoader(
+                    file_path,
+                    encoding="utf-8"
+                )
+
+                documents.extend(
+                    loader.load()
+                )
+
+            # =====================================
+            # PDF FILES
+            # =====================================
+
+            elif file_name.endswith(".pdf"):
+
+                loader = PyPDFLoader(
+                    file_path
+                )
+
+                documents.extend(
+                    loader.load()
+                )
+
+        except Exception as error:
+
+            logging.warning(
+                f"Failed to load "
+                f"{file_name}: {error}"
             )
 
-            loader = TextLoader(
-                file_path,
-                encoding="utf-8"
-            )
-
-            documents.extend(
-                loader.load()
-            )
+            # SKIP BAD FILES
+            continue
 
     return documents
